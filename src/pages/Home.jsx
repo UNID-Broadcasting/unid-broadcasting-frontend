7884846d80dc4e24c3bc65f9e801e350ead18f2d
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-18 00:54:34 -0600

    Component and Home Page added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <h2>Home</h2>
+    </>
+  )
+}
+
+export default Home
