d935d1588b5876e3da7bf1ef8699446bfe33373b
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-26 02:23:33 -0600

    API implemented, Props and Axios Installed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Horarios from '../components/Horarios'
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Horarios from "../components/Horarios";
 
 const Home = () => {
-	return (
-		<>
-			<Header />
-			<main className='pt-20'>
-				<Hero />
-				<div>
-					<Horarios />
-				</div>
-			</main>
-		</>
-	)
-}
+  const [fecha, setFecha] = useState({});
 
-export default Home
+  const obtenerFecha = async () => {
+    const data = await fetch(
+      "https://unid-backend-radioytv.onrender.com/api/date"
+    );
+    const fecha = await data.json();
+    setFecha(fecha);
+    console.log(fecha);
+  };
+
+  useEffect(() => {
+    obtenerFecha();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main className="pt-20">
+        <Hero />
+        <div>
+          <Horarios day={fecha.day} month={fecha.monthName} year={fecha.year} />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
