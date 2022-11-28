a0bbca4e3eb864092546bccb11b9138ee9ef3172
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-28 00:41:10 -0600

    Team Page added and Router

---
+++ src/components/OurTeam.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+
+const OurTeam = () => {
+  return (
+    <>
+      <div className="col-span-4">
+        <h2 className="text-2xl font-bold text-center my-8">Radio</h2>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/men/73.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/men/3.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/women/27.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/women/72.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/men/25.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/women/50.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/lego/5.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="col-span-4">
+          <h2 className="text-2xl font-bold text-center my-8">
+            Desarrolladores
+          </h2>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/men/73.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/men/3.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/women/27.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/women/72.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/men/25.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/women/50.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            className="rounded-full w-32 h-32"
+            src="https://randomuser.me/api/portraits/lego/5.jpg"
+            alt="team"
+          />
+          <h3 className="text-xl font-bold mt-2">John Doe</h3>
+          <p className="text-gray-500">CEO</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OurTeam;
---
+++ src/pages/Team.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import OurTeam from "../components/OurTeam";
+
+const Team = () => {
+  return (
+    <main className="mt-40">
+      <OurTeam />
+    </main>
+  );
+};
+
+export default Team;
