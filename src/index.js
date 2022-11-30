36085bb07268bc06181d9f8155c1b19316914d02
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-30 15:55:19 -0600

    Google Analitycs added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "../src/styles/index.css";
 import App from "./App";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("G-BM5NT3CD18");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
