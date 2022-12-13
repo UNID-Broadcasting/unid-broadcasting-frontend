2f7ff8b328b8c117d353b33af938e145deb09b29
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-12-13 15:17:48 -0600

    Modal made with Tailwind

--- src/components/PodcastList.jsx
+++ src/components/PodcastList.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const PodcastList = () => {
   const [podcasts, setPodcasts] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [modalData, setModalData] = useState(null);
 
   useEffect(() => {
     const obtenerPodcasts = async () => {
@@ -15,30 +18,94 @@ const PodcastList = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-4">
-      <h2 className="text-2xl font-bold text-center my-8">Podcasts</h2>
-      <p className="text-center text-gray-600 mb-8">
-        Disfruta de los mejores podcasts en un solo lugar. Estás en el lugar
-        ideal para disfrutar de contenido diseñado exclusivamente para ti.
-      </p>
-      <div className="flex overflow-x-scroll">
-        {podcasts.map((podcast, id) => (
-          <div className="flex-shrink-0 m-8" key={id}>
-            <img
-              className="h-48 w-48 rounded-lg object-cover"
-              src={podcast.image}
-              alt="Podcast"
-            />
-            <div className="mt-2">
-              <p className="text-lg text-gray-900 w-48">{podcast.name}</p>
-              <p className="text-gray-600 text-sm w-48">
-                {podcast.description}
-              </p>
+    <>
+      <div className="container mx-auto px-4">
+        <h2 className="text-2xl font-bold text-center my-8">Podcasts</h2>
+        <p className="text-center text-gray-600 mb-8">
+          Disfruta de los mejores podcasts en un solo lugar. Estás en el lugar
+          ideal para disfrutar de contenido diseñado exclusivamente para ti.
+        </p>
+        <div className="flex overflow-x-scroll">
+          {podcasts.map((podcast, id) => (
+            <Link
+              key={id}
+              onClick={() => {
+                setShowModal(true);
+                setModalData(podcast);
+              }}
+            >
+              <div className="flex-shrink-0 m-8" key={id}>
+                <img
+                  className="h-48 w-48 rounded-lg object-cover"
+                  src={podcast.image}
+                  alt="Podcast"
+                />
+                <div className="mt-2">
+                  <p className="text-lg text-gray-900 w-48">{podcast.name}</p>
+                  <p className="text-gray-600 text-sm w-48">
+                    {podcast.description}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    {modalData ? modalData.name : ""}
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    {modalData ? modalData.description : ""}
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cerrar
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 flex justify-between items-center"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Escuchalo en spotify
+                    <img
+                      className="h-6 w-6 rounded-lg object-cover ml-2"
+                      src="https://cdn-icons-png.flaticon.com/512/152/152756.png"
+                      alt="Spotify"
+                    />
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        ))}
-      </div>
-    </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
   );
 };
 
