ad8f4619025735689e04044247a6522e530b6c15
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-12-16 01:11:23 -0600

    Captcha, SweetAlert, SignUp Page, and Route to Create Account

---
+++ src/pages/Singup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUpForm from "../components/SignUpForm";
+
+const Singup = () => {
+  return (
+    <>
+      <SignUpForm />
+    </>
+  );
+};
+
+export default Singup;
