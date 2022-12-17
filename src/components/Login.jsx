4c9a78073eb6948f1137c402967a1c32b641bab5
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-12-17 10:24:30 -0600

    Login link changed, styles added in signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -123,7 +123,7 @@ const Login = () => {
                   <p className="text-sm font-semibold mt-2 pt-1 mb-0">
                     ¿No tienes una cuenta?
                     <Link
-                      to="/registro"
+                      to="/signup"
                       className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
                     >
                       Regístrate
