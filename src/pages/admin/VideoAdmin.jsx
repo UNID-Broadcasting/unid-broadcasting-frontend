80838e30f9f3e4ad00f53205cd76e4c9bd4d012b
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-12-25 23:28:43 -0600

    Meta Tag Added and Fixed

--- src/pages/admin/VideoAdmin.jsx
+++ src/pages/admin/VideoAdmin.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Videos from "../../components/admin/Videos/Videos";
 
 const VideoAdmin = () => {
   return (
     <>
+      <Helmet>
+        <title>Videos Dashboard - UNID Radio y Televisión</title>
+        <meta
+          name="description"
+          content="Bienvenido al apartado de videos, espacio donde podrás agregar nuevos videos, editarlos o eliminarlos"
+        />
+      </Helmet>
       <Videos />
     </>
   );
