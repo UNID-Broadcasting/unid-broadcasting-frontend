6c5e3131f9b0df6d54d6174fa36dc9667033c5e3
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-12-31 20:52:44 -0600

    New music player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,30 +1,20 @@
 import React from "react";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+const urlSrc =
+  "https://Fps3.listen2myradio.com:2199/listen.php?ip=109.169.23.124&port=8399&type=s1";
 
 const Player = () => {
   return (
     <>
-      <div className="bg-gray-800 w-full h-16 fixed bottom-0 flex items-center justify-between">
-        <div className="flex items-center">
-          <div className="w-16 h-16 bg-gray-700">
-            <img
-              src="https://img.freepik.com/vector-premium/ilustracion-icono-auriculares-musica-telefono-inteligente-concepto-tecnologia-aislado_138676-1107.jpg"
-              alt="logo"
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div className="ml-4">
-            <p className="text-white">Radio</p>
-            <p className="text-gray-400"></p>
-          </div>
-        </div>
-        <div className="flex items-center mr-8">
-          {/* Escucharemos al servidor 109.169.23.124 y puerto 8399 */}
-          <audio
-            src="https://Fps3.listen2myradio.com:2199/listen.php?ip=109.169.23.124&port=8399&type=s1"
-            controls
-            autoPlay
-          ></audio>
-        </div>
+      <div>
+        <AudioPlayer
+          autoPlay={true}
+          src={urlSrc}
+          showJumpControls={false}
+          hasDefaultKeyBindings={false}
+          autoPlayAfterSrcChange={true}
+        />
       </div>
     </>
   );
