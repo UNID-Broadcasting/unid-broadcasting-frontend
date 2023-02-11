1d23f9fd5a82201bd2095588420da7b14d65b182
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-02-11 11:44:55 -0600

    URL in button after click redirect to a new tab

--- src/components/PodcastList.jsx
+++ src/components/PodcastList.jsx
@@ -97,7 +97,9 @@ const PodcastList = () => {
                   <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 flex justify-between items-center"
                     type="button"
-                    onClick={() => setShowModal(false)}
+                    onClick={() =>
+                      window.open(modalData.url, "_blank") & setShowModal(false)
+                    }
                   >
                     Escúchalo en Spotify
                     <img
